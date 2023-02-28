a9e4dac449739c7b607b56c7fd48a7980c99e77c
SuragVC <suragvc2000@gmail.com>
2023-02-28 22:01:31 +0530

    sign in page added

--- FrontEnd/cypro-app/src/pages/SignIn.jsx
+++ FrontEnd/cypro-app/src/pages/SignIn.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
-
+import React from "react";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import "./style/SignIn.css";
+import { Button } from "@mui/material";
 export default function SignIn() {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
   return (
-    <div>SignIn</div>
-  )
+    <Box className="centerBox signinDiv">
+      <Box className="signinBox">
+        <TextField
+          sx={{ m: 1, width: "25ch" }}
+          helperText="Please enter your email"
+          id="demo-helper-text-aligned"
+          label="Email"
+        />
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        <Box className="centerBox">
+          <Button variant="contained">Submit</Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
