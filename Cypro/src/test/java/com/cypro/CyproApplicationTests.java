d3af2b395e27d714e2e9080a8455e0cb40a9b1e5
SuragVC <suragvc2000@gmail.com>
2023-03-03 21:17:35 +0530

    new backend created

---
+++ Cypro/src/test/java/com/cypro/CyproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
