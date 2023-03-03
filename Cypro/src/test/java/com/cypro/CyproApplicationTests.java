3f228e302536c782c31bb226d9e0c7da78fd20c0
SuragVC <suragvc2000@gmail.com>
2023-03-03 21:36:23 +0530

    Unit testing added

--- Cypro/src/test/java/com/cypro/CyproApplicationTests.java
+++ Cypro/src/test/java/com/cypro/CyproApplicationTests.java
@@ -1,13 +1,29 @@
 package com.cypro;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import com.cypro.controller.TestController;
 
 @SpringBootTest
 class CyproApplicationTests {
+	@Autowired
+	private TestController testController;
 
 	@Test
 	void contextLoads() {
+		ResponseEntity<String> ans=testController.test();
+		assertNotNull(ans);
 	}
+	@Test
+	void LoadTest() {
+		ResponseEntity<String> ans=testController.test();
+		assertEquals("working",ans.getBody());
+	}
+	
 
 }
